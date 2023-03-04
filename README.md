0e8a93dcc34a1d598cf37c5f79b60ee27d1b03a2
Sathi <ssathi@protonmail.com>
2023-03-04 14:58:00 +0800

    apis and read me update

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # boutique-hotel
 A project for Telepathy AI
+
+# Instructions
+
+1. Install JDK 17 or later
+2. Run './mvnw clean test' to run the test cases
+3. Run './mvnw clean install' to run the test cases and build the runnable jar
+4. After successful build, run 'java -jar target/boutique-hotel-1.0.jar'
+
+
+# Testing the App
+Open another terminal to perform below tests
+
+1. To check in, run 'curl -X POST http://localhost:9999/hotel/room/checkin'
+2. To check out, replace 'ROOM ID' from previous command and run 'curl -X POST http://localhost:9999/hotel/room/{ROOM ID}/checkout'
+e.g. curl -X POST http://localhost:9999/hotel/room/1A/checkout
+
+3. To clean, replace 'ROOM ID' from previous command and run 'curl -X POST http://localhost:9999/hotel/room/{ROOM ID}/clean'
+e.g. curl -X POST http://localhost:9999/hotel/room/1A/clean
+
+4. To mark a room for repair, replace 'ROOM ID' from previous command and run 'curl -X POST http://localhost:9999/hotel/room/1A/outofservice'
+e.g. curl -X POST http://localhost:9999/hotel/room/1A/outofservice
+
+5. To query all available rooms, run 'curl  http://localhost:9999/hotel/room'
+
--- src/main/java/com/example/boutiquehotel/controller/BookingController.java
+++ src/main/java/com/example/boutiquehotel/controller/BookingController.java
@@ -5,9 +5,9 @@ import com.example.boutiquehotel.exception.RoomNotFoundException;
 import com.example.boutiquehotel.service.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class BookingController {
@@ -23,5 +23,20 @@ public class BookingController {
     @PostMapping("/hotel/room/{id}/checkout")
     public ResponseEntity<Room> checkout(@PathVariable String id) throws RoomNotFoundException {
         return ResponseEntity.ok(service.checkout(id));
+    }
+
+    @PostMapping("/hotel/room/{id}/clean")
+    public ResponseEntity<Room> clean(@PathVariable String id) throws RoomNotFoundException {
+        return ResponseEntity.ok(service.clean(id));
+    }
+
+    @PostMapping("/hotel/room/{id}/outofservice")
+    public ResponseEntity<Room> outOfService(@PathVariable String id) throws RoomNotFoundException {
+        return ResponseEntity.ok(service.outOfService(id));
+    }
+
+    @GetMapping ("/hotel/room")
+    public ResponseEntity<List<Room>> getAvailableRooms() {
+        return ResponseEntity.ok(service.findAllAvailable());
     }
 }
