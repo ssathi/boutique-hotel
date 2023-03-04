b302cd4bf23992ba875603073d30312a9df4328f
Sathi <ssathi@protonmail.com>
2023-03-04 12:53:35 +0800

    Project Initial Setup

---
+++ src/main/java/com/example/boutiquehotel/BoutiqueHotelApplication.java
@@ -0,0 +1,13 @@
+package com.example.boutiquehotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoutiqueHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoutiqueHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/boutiquehotel/BoutiqueHotelApplicationTests.java
@@ -0,0 +1,9 @@
+package com.example.boutiquehotel;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoutiqueHotelApplicationTests {
+
+
+}
